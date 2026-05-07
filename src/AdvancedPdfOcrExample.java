1156a77c4992d35129d104496862fc0fd95f5b38
yangjunsig <wnstlr1379@naver.com>
2026-05-07 22:48:06 +0900

    PDF 종속성 추가

---
+++ src/AdvancedPdfOcrExample.java
@@ -0,0 +1,10 @@
+import java.time.Duration;
+
+public class AdvancedPdfOcrExample {
+    private static final int OCR_DPI = 200;
+    private static final Duration OCR_TIMEOUT = Duration.ofSeconds(30);
+
+    public static void main(String[] args) {
+
+    }
+}
